23b4a613546f211a41c932856f6b6b0bbfd63ac5
FabiAlonso <fabi.alonso.orozco@gmail.com>
2025-04-24 14:51:05 -0400

    Changes to meal_price and tip_options

--- assingment1.py
+++ assingment1.py
@@ -1,7 +1,7 @@
 #Tip calculator: this program calculates the price per person for a meal
 
 diners = int(input("How many diners did you have?"))
-meal_price = float(input("How much was the diner?"))
+meal_price = float((input("How much was the diner?").replace(',','.'))) #ensures the quantity is separated by a decima pont not by a comma.
 GST = 0.05 #Federal tax
 QST = 0.0975 #Regional tax
 meal_price_GST = meal_price * GST
@@ -23,19 +23,19 @@ print(f"You have selected {tip_selected}")
 if tip_selected == "1":
     tip_percentage = 0.02
     tip_amount = meal_price *tip_percentage
-    print("The tip amount is $", tip_amount, "***Thank you!****")
+    print(f"The tip amount is ${tip_amount:.2f} ***Thank you!****")
 elif tip_selected == "2":
     tip_percentage = 0.015
     tip_amount = meal_price * tip_percentage
-    print("The tip amount is $", tip_amount)
+    print(f"The tip amount is ${tip_amount:.2f}")
 elif tip_selected == "3":
     tip_percentage = 0.01
     tip_amount = meal_price * tip_percentage
-    print("The tip amount is $", tip_amount)
+    print(f"The tip amount is ${tip_amount:.2f}")
 elif tip_selected == "4":
     tip_percentage = 0.0
     tip_amount = meal_price * tip_percentage
-    print("The tip amount is $", tip_amount)
+    print(f"The tip amount is ${tip_amount:.2f}")
 
 grand_total = total + tip_amount
 print(f"The GST added is ${meal_price_GST:.2f}")
