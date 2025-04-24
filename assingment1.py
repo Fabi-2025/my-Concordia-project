0e62b7b27bb02bf0ed862c524029817334cf8de7
FabiAlonso <fabi.alonso.orozco@gmail.com>
2025-04-24 09:56:52 -0400

    None for the moment

--- assingment1.py
+++ assingment1.py
@@ -7,40 +7,47 @@ QST = 0.0975 #Regional tax
 meal_price_GST = meal_price * GST
 meal_price_QST = meal_price * QST
 total =meal_price + meal_price_GST +meal_price_QST
-print("The GST added is $",meal_price_GST)
-print("The QST added is $", meal_price_QST)
-print(f"The total of your diner including taxes is ${total:.2f}")
-print("1) Exceptional 20%, 2)Good 15%, 3) Basic 10%, 4)Horrible 0%")
-tip = input("Please select the tip you would like to give: ")
+tip_options = ["1","2","3","4"]
 
-#This loop will keep asking the user to enter a number as long as the number is not between 1-4.
-while tip '1','2','3','4',q'):
-    if tip == q:
-        print("Bye")
-     #print("Please select the tip you want to give (type 1, 2, 3 or 4): ")
-     #tip = int(input("Please select the tip you would like to give: "))
-    tip = int(tip)
-if tip == 1:
+while True: #The code will run within this loop until the tip typed matches the tip_options
+    tip_selected = input("Please select the tip you would like to give: "
+                    "1) Exceptional 20%, 2)Good 15%, 3) Basic 10%, 4)Horrible 0%")
+
+    #If letter q is typed, then print bye and exit the loop
+
+    if (tip_selected in tip_options):
+        break
+    print("Input invalid, please select the tip you want to give (type 1, 2, 3 or 4): ")
+
+print(f"You have selected {tip_selected}")
+if tip_selected == "1":
     tip_percentage = 0.02
     tip_amount = meal_price *tip_percentage
     print("The tip amount is $", tip_amount, "***Thank you!****")
-elif tip == 2:
+elif tip_selected == "2":
     tip_percentage = 0.015
     tip_amount = meal_price * tip_percentage
-    print("The tip amount is $", tip_amount, "***Thank you!****")
-elif tip == 3:
+    print("The tip amount is $", tip_amount)
+elif tip_selected == "3":
     tip_percentage = 0.01
     tip_amount = meal_price * tip_percentage
-    print("The tip amount is $", tip_amount, "***We will improve next time****")
-elif tip == 4:
+    print("The tip amount is $", tip_amount)
+elif tip_selected == "4":
     tip_percentage = 0.0
     tip_amount = meal_price * tip_percentage
-    print("The tip amount is $", tip_amount, "***Sorry! Hope you come back****")
+    print("The tip amount is $", tip_amount)
 
 grand_total = total + tip_amount
-print("The total of your diner including tip is $", grand_total)
+print(f"The GST added is ${meal_price_GST:.2f}")
+print(f"The QST added is ${meal_price_QST:.2f}")
+print(f"The total of your diner including taxes is ${total:.2f}")
+print(f"The total of your diner including tip is $ {grand_total:.2f}")
 
 
+# import pandas as pd
+#
+# df = pd.read_csv('50000_Sales_Records.csv')
+# print(df.head())
 
 
 
